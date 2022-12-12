f8512a0a39677dd1e56b1f50427bbc2c85ce5a1a
Chicho <chichorubio@hotmail.com>
2022-12-12 20:41:40 -0300

    Inicio proyecto ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function CollapsibleExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Club UVAFO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#inicio">Inicio</Nav.Link>
+            <Nav.Link href="#novedades">Novedades</Nav.Link>
+            <NavDropdown title="Institución" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#deportes">
+                Deportes
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#asociarse">
+                ¡Asociate ahora!
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#pptoparticipativo">
+                Ppto. Participativo 2022
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#contacto">
+                Contacto
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://www.instagram.com/clubuvafo/">
+              Instagram
+            </Nav.Link>
+            <Nav.Link eventKey={2} href="https://www.youtube.com/@ClubUVAFO">
+              Youtube
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default App;
+export default CollapsibleExample;
---
+++ src/components/button/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css';
+<styles className="css"></styles>
+const Button = () => {
+    return (
+        <button className="button1">Click me</button>
+    )
+}
+
+export default Button;
